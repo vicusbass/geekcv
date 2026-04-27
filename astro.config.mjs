8ba9274c6c49052d962df96fe40fcea0cfe57200
Vasile Pop <vasile.pop@gmail.com>
2026-04-27 09:36:01 +0300

    canonical

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://vicusbass.com',
+  site: 'https://www.vicusbass.com',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
